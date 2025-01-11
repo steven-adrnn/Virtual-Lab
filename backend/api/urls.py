110643559df54fcf33a8da7b9f0fc8f51b3ad85c
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2025-01-12 03:20:19 +0700

    .

---
+++ backend/api/urls.py
@@ -0,0 +1,18 @@
+# api/urls.py
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, QuizViewSet, QuizProgressView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'quizzes', QuizViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('quiz-progress/', QuizProgressView.as_view(), name='quiz-progress'),
+]
